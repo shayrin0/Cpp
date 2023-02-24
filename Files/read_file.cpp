50b5c267057ea53bcf6614edd52affe2d10597d6
shayrin0 <shayrin.panahi@gmail.com>
2023-02-23 19:45:59 -0500

    add openFile function

--- Files/read_file.cpp
+++ Files/read_file.cpp
@@ -5,13 +5,30 @@
 
 using namespace std;
 
+void openFile(ifstream&, string);
+
 int main()
 {
     // 1. open the file
+    ifstream inFile;
+    openFile(inFile, "sentence.txt");
 
     // 2. process the file
 
     // 3. close the file
 
     return 0;
+}
+
+void openFile(ifstream& inFile, string fname)
+{
+    inFile.open(fname);
+
+    if (inFile.is_open())
+        cout << "Successfully opened file!" << endl;
+    else
+    {
+        cout << "Failed to open file!" << endl;
+        exit(-1);
+    }
 }

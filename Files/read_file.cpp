cebebb90dd4c6d004391d3b16746c744e38b4e1d
shayrin0 <shayrin.panahi@gmail.com>
2023-02-23 19:50:14 -0500

    read file line by line

--- Files/read_file.cpp
+++ Files/read_file.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 void openFile(ifstream&, string);
+void processFile(ifstream&);
 
 int main()
 {
@@ -14,6 +15,7 @@ int main()
     openFile(inFile, "sentence.txt");
 
     // 2. process the file
+    processFile(inFile);
 
     // 3. close the file
     inFile.close();
@@ -31,5 +33,21 @@ void openFile(ifstream& inFile, string fname)
     {
         cout << "Failed to open file!" << endl;
         exit(-1);
+    }
+}
+
+void processFile(ifstream& inFile)
+{
+    string line;
+
+    while(!inFile.eof()) // eof = end of file
+    {
+        // read line by line from the file
+        getline(inFile, line);
+
+        if (inFile.good())
+        {
+            cout << line << endl;
+        }
     }
 }

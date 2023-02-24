2bde9699f6b6eca8362e4ce455f724430e18e49e
shayrin0 <shayrin.panahi@gmail.com>
2023-02-23 19:42:16 -0500

    add main function

--- Files/read_file.cpp
+++ Files/read_file.cpp
@@ -2,3 +2,16 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
+using namespace std;
+
+int main()
+{
+    // 1. open the file
+
+    // 2. process the file
+
+    // 3. close the file
+
+    return 0;
+}

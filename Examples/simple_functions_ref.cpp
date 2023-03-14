6c4060068bc1e0b144a61fdeb0a42ffa40168dcb
shayrin0 <shayrin.panahi@gmail.com>
2023-03-14 12:38:07 -0400

    initial commit

---
+++ Examples/simple_functions_ref.cpp
@@ -0,0 +1,16 @@
+
+/* References example */
+
+#include <iostream>
+
+using namespace std;
+
+//Add two numbers and return the result through a reference parameter
+void Add(int a,int b, int &result) ;    
+
+// Find factorial of a number and 
+// return that through a reference parameter
+void Factorial(int a, int &result) ;    
+
+//Swap two numbers through reference arguments
+void Swap(int &a, int &b) ;            

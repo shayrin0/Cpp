fdae87b6530e89d6c779b4c917dedc6236a23785
shayrin0 <shayrin.panahi@gmail.com>
2023-03-14 12:05:07 -0400

    initialize arrays and strings

--- Initialization/init_var.cpp
+++ Initialization/init_var.cpp
@@ -14,5 +14,13 @@ int main()
     int a2 = 0; // copy initialization
     int a3(5); // direct initialization
 
+    string s1;
+    string s2("C++"); // direct initialization
+
+    char d1[8];
+    char d2[8] = {'\0'};
+    char d3[8] = {'a', 'b', 'c'}; // aggregate initialization
+    char d4[8] = {"abcd"};
+
     return 0;
 }

0ed9496145364861612f514aeee4ff155f3006c9
shayrin0 <shayrin.panahi@gmail.com>
2023-03-14 13:46:03 -0400

    assign pointer to a function

--- Examples/func_pointer.cpp
+++ Examples/func_pointer.cpp
@@ -7,10 +7,21 @@ void Print (int count, char ch)
 {
     for (int i=0; i<count; i++)
         cout << ch;
+    cout << endl;
+}
+
+void EndMessage()
+{
+
 }
 
 int main()
 {
+    Print(5, '#');
+
+    void (*ptr)(int, char) = Print;
+    (*ptr)(8, '*');
+    ptr(3, '@');
 
     return 0;
 }
